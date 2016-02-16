35326a596635a3d2df458806d200822ee936c3f2
amandakifer <amandakifer@gmail.com>
2016-02-16 14:03:37 -0800

    Added redirect.

--- eat_local/urls.py
+++ eat_local/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 from deals import urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-        url(r'^deals/', include(urls), name='deals'),
+    url(r'^$', RedirectView.as_view(url='deals', permanent=True)),
+    url(r'^deals/', include(urls), name='deals'),
 ]
