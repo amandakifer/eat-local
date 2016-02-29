cf6cbdf0af8ac3748fa6ac73f874e5d6530c4b6b
amandakifer <amandakifer@gmail.com>
2016-02-29 11:53:29 -0800

    Added css file and installed static css setttings

--- eat_local/urls.py
+++ eat_local/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import RedirectView
 
 from deals import urls as deals_urls
@@ -9,3 +10,4 @@ urlpatterns = [
     url(r'^$', RedirectView.as_view(url='deals', permanent=True)),
     url(r'^deals/', include(deals_urls), name='deals'),
 ]
+urlpatterns += staticfiles_urlpatterns()
