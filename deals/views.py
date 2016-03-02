0de5adcf4450a9d3ec15d2a921fe94830bb12552
amandakifer <amandakifer@gmail.com>
2016-03-02 09:30:09 -0800

    Added result pagination

--- deals/views.py
+++ deals/views.py
@@ -1,4 +1,8 @@
+from urllib import urlencode
+
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render
+
 from libs.wrapper import Yelp
 from deals import forms as deals_forms
 
@@ -12,8 +16,21 @@ def index(request):
             search_terms = form.cleaned_data['search_terms']
             location = form.cleaned_data['location']
             context['location'] = location
+            context['url'] = '?%s' % urlencode({'search_terms': search_terms, 'location': location})
             results = yelp_wrapper.search(search_terms, location, deals_filter=True)
-            context['results'] = results
+
+            paginator = Paginator(results.get('businesses'), 5)
+            page = request.GET.get('page')
+            try:
+                businesses = paginator.page(page)
+            except PageNotAnInteger:
+                # If page is not an integer, deliver first page.
+                businesses = paginator.page(1)
+            except EmptyPage:
+                # If page is out of range (e.g. 9999), deliver last page of results.
+                businesses = paginator.page(paginator.num_pages)
+
+            context['businesses'] = businesses
     else:
         form = deals_forms.LocationForm()
 
